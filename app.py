fd78da952b2c4e047148fc36fa96ec81fead2596
tomer18-meet <redtomeradar@gmail.com>
2018-11-08 18:44:23 +0200

    s

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask import render_template
-from database import get_all_cats
+from database import get_all_cats, create_cat, get_cat_by_id
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def catbook_home():
     cats = get_all_cats()
-    return render_template("home.html", cats=cats)
+    if request.method == 'POST':
+        name = request.form['name']
+        image = request.form['image']
+        create_cat(name, image)
+        return redirect(url_for('catbook_home'))
+    else:
+        return render_template("home.html", cats=cats)
+
+@app.route('/cats/<int:id>')
+def cats(id):
+
+    cat = get_cat_by_id(id)
+    return render_template(
+
+        'cat.html',
+        cat = cat
+
+        )
+
+@app.route('/search', methods=['GET'.'POST'])
+def search():
+	if request.method == "POST":
+		query = request.form['search']
+
+
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
--- database.py
+++ database.py
@@ -8,11 +8,19 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def create_cat(name):
-    cat_object = Cat(name=name)
+def create_cat(name, image):
+    cat_object = Cat(name = name, image = image)
     session.add(cat_object)
     session.commit()
 
 def get_all_cats():
     cats = session.query(Cat).all()
     return cats
+
+def get_cat_by_id(id):
+    cat = session.query(Cat).filter_by(id = id).first()
+    return cat
+
+def get_cat_by_name(name):
+	cat = session.query(Cat).filter_by(name = name).all()
+	return cat
